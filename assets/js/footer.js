d8324b3912393b40271dc3fe1dc1bcf4a45289b5
jonodjlee <jonodjlee.github@gmail.com>
2021-03-12 01:13:31 +1100

    Update footer.js

--- assets/js/footer.js
+++ assets/js/footer.js
@@ -2,7 +2,7 @@ function renderfooter() {
     var a = (new Date().getFullYear());
     var template = document.getElementById('copyright').innerHTML;
     var rendered = Mustache.render(template, {
-        copyright: ('© ' + a + ' Peapeed. All rights reserved.')
+        copyright: ('© ' + a + ' SESS. All rights reserved.')
     });
     document.getElementById('copyright').innerHTML = rendered;
 }
